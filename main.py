ded332fb207d864afdec982141c093062e590967
Ingrid Lima <ibezerradelima@gmail.com>
2024-03-26 21:23:05 -0300

    feat: Add the similarity method

--- main.py
+++ main.py
@@ -4,15 +4,34 @@ Graph = graph.Graph()
 Utils = utils.Utils()
 
 file1, file2 = Utils.readGraphs()
+similarityMethod = Utils.readSimilarityMethod()
 
 graph1 = graph.Graph()
 graph2 = graph.Graph()
 
-graph1.fromAdjacencyMatrix(Graph.readAdjacencyMatrixFromFile('Files/graph1.txt'))
-graph2.fromAdjacencyMatrix(Graph.readAdjacencyMatrixFromFile('Files/graph2.txt'))
+graph1.setAdjacencyMatrix(Graph.readAdjacencyMatrixFromFile(file1))
+graph2.setAdjacencyMatrix(Graph.readAdjacencyMatrixFromFile(file2))
 
+print("----------------------------------------------------------------------------------------")
 
+print("The adjacency matrices of the graphs are:")
+
+print("Graph 1", graph1.adjMatrix)
+print("Graph 2", graph2.adjMatrix)
+
+similarity = graph1.isSimilar(graph2, similarityMethod)
+
+message = "The graphs "
 if graph1.isIsomorphic(graph2):
-    print("Os grafos são isomorfos.")
+    message = message + ("are isomorphic and")
 else:
-    print("Os grafos não são isomorfos.")
+    message = message +  ("are not isomorphic and")
+
+if  similarity > 0.75:
+    message = message + (" are similar.")
+else:
+    message = message + (" are not similar.")
+
+print("Have a similarity of:", similarity, "using the method", similarityMethod)
+print(message)
+print("----------------------------------------------------------------------------------------")
