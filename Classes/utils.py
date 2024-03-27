b5b539a1077b66b3e6e9e8b4ce9b3bd7288f64ae
Ingrid Lima <ibezerradelima@gmail.com>
2024-03-26 21:22:30 -0300

    :recycle: refactor: Utils class
    Messages translated from the readGraph method and created the readSimilarity method.

--- Classes/utils.py
+++ Classes/utils.py
@@ -1,14 +1,27 @@
 class Utils:
   def readGraphs(self):
-    choice = input("Deseja inserir os caminhos manualmente ou ler os arquivos já salvos? (1/2): ")
+    choice = input("Do you want to enter the paths manually(1) or read already saved files(2)? (1/2): ")
     if choice == '1':
-        file1 = input("Digite o caminho do primeiro arquivo: ")
-        file2 = input("Digite o caminho do segundo arquivo: ")
+        file1 = input("Enter the path of the first file: ")
+        file2 = input("Enter the path of the second file: ")
+
         return file1, file2
     
     elif choice == '2':
-        return 'File/graph1.txt', 'File/graph2.txt'
+        return 'Files/graph1.txt', 'Files/graph2.txt'
     
     else:
-        print("Opção inválida. Por favor, escolha '1' para ler de arquivos salvos ou '2' para inserir os caminhos pelo terminal.")
+        print("Invalid option. Choose '1' to read saved files or '2' to enter paths via terminal.")
         return self.readGraphs()
+    
+  def readSimilarityMethod(self):
+     choice = input("Choose the method for calculating similarity (1 - cosine, 2 - simRank, 3 - jaccard):")
+     if choice == '1':
+         return 'cosine'
+     elif choice == '2':
+         return 'simRank'
+     elif choice == '3':
+         return 'jaccard'
+     else:
+         print("Invalid option. Choose '1', '2' or '3'.")
+         return self.readSimilarityMethod()
